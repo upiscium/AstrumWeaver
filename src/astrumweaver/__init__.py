26af64a233c2cac6305af575e362097056d80ea0
upiscium <130824002+upiscium@users.noreply.github.com>
2026-09-23 10:50:10 +0900

    feat: export generic execution contracts

--- src/astrumweaver/__init__.py
+++ src/astrumweaver/__init__.py
@@ -1,11 +1,25 @@
 """AstrumWeaver domain contracts."""
 
 from .contracts import JobRequirements, ResourceShape, WorkerSpec
+from .execution import (
+    ArtifactRef,
+    JobExecutor,
+    JobRequest,
+    JobResult,
+    ResidencyItem,
+    ResidencyReport,
+)
 from .scheduling import MatchResult, match_worker, worker_matches
 
 __all__ = [
+    "ArtifactRef",
+    "JobExecutor",
+    "JobRequest",
     "JobRequirements",
+    "JobResult",
     "MatchResult",
+    "ResidencyItem",
+    "ResidencyReport",
     "ResourceShape",
     "WorkerSpec",
     "match_worker",
